315742de644f039cd16ea84287a1c9ec1a82f8d3
dvianello <dario.vianello87@gmail.com>
2015-11-02 15:55:27 +0000

    Initial cloud_testing.sh commit

---
+++ cloud_testing.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# User requires sudo rights
+
+#TODO: use an ad-hoc folder named after the cloud we're testing, or add a
+#prefix to all files
+LOG="~/cloud_testing/kv-script.sh.out"
