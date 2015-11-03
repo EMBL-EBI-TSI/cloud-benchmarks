3f951624703dbecdd65866da8994720f30e0be65
dvianello <dario.vianello87@gmail.com>
2015-11-03 10:02:58 +0000

    Add first draft script with Phoronix.

--- cloud_testing.sh
+++ cloud_testing.sh
@@ -4,4 +4,76 @@
 
 #TODO: use an ad-hoc folder named after the cloud we're testing, or add a
 #prefix to all files
-LOG="~/cloud_testing/kv-script.sh.out"
+LOG="$HOME/cloud_testing/cloud_testing.log"
+
+if [ -d "$HOME/cloud_testing" ]; then
+  echo "WARNING: old cloud_testing logs found. Getting rid of them"
+  rm -r ~/cloud_testing
+fi
+
+if [ -d "$HOME/phoronix-test-suite" ]; then
+  echo "WARNING: old phoronix-test-suite folder found. Getting rid of it."
+  rm -rf ~/phoronix-test-suite
+fi
+
+mkdir ~/cloud_testing
+
+#Redirect STDOUT to log file
+exec 1>$LOG 2>&1
+
+# Exit when any command fails. To allow failing commands, add "|| true"
+set -o errexit
+
+
+function install_dependencies() {
+  #Update yum cache
+  sudo yum makecache fast
+
+  #Phoronix
+  sudo yum -y install git php php-xml xdg-utils
+}
+
+
+function install_phoronix() {
+  # get the latest stable version of phoronix test suite
+  git clone https://github.com/phoronix-test-suite/phoronix-test-suite.git
+
+  # Add pts to local path
+  PATH="$PATH:$HOME/phoronix-test-suite"
+
+  # Accept terms of pts (Y)
+  echo "Y" | phoronix-test-suite batch-setup
+
+  # Collect information about local system
+  # phoronix-test-suite system-info
+
+  # Configure pts to run in batch mode
+  sed -i \
+  -e 's/<SaveResults>FALSE/<SaveResults>TRUE/' \
+  -e 's/<OpenBrowser>TRUE/<OpenBrowser>FALSE/' \
+  -e 's/<UploadResults>TRUE/<UploadResults>FALSE/' \
+  -e 's/<PromptForTestIdentifier>TRUE/<PromptForTestIdentifier>FALSE/' \
+  -e 's/<PromptForTestDescription>TRUE/<PromptForTestDescription>FALSE/' \
+  # -e 's/<PromptSaveName>FALSE/<PromptSaveName>TRUE/' \
+  -e 's/<PromptSaveName>TRUE/<PromptSaveName>FALSE/' \
+  -e 's/<RunAllTestCombinations>FALSE/<RunAllTestCombinations>TRUE/' \
+  -e 's/<Configured>FALSE/<Configured>TRUE/' \
+  ~/.phoronix-test-suite/user-config.xml
+
+  # Install the dependency for the test
+  phoronix-test-suite install sqlite
+  # Run the test and save the results as batch_tests
+  # echo "cloudtests" | phoronix-test-suite batch-benchmark sqlite
+   TEST_RESULTS_NAME="cloudtests" phoronix-test-suite batch-benchmark sqlite
+  # Run more benchmarks in a run
+  # echo "cloudtests" | phoronix-test-suite batch-benchmark smallpt build-linux-kernel c-ray sqlite fourstones pybench
+  # Export results in json file (use cvs if php < 5.4, i.e. Centos 6)
+  phoronix-test-suite  result-file-to-json cloudtests > ~/cloudtests.json
+}
+
+# MAIN
+echo "STEP 1 - Install tools and dependencies"
+echo "INSTALLING DEPENDENCIES"
+install_dependencies
+echo " INSTALLING PHORONIX TEST SUITE"
+install_phoronix
